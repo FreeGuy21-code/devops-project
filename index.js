3e89974fdb036e81d39cf5725ff29149a296c98f
Jeet Jain <jeetjain0721@gmail.com>
2025-03-16 19:49:23 +0530

    Added full HTML webpage

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("HELLO THIS IS MY FIRST DEV OPS PROJECT!"));
+const path = require("path");
+
+app.use(express.static("public")); // Serve static files
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(3000, () => console.log("App running on port 3000"));
+
